5478f8734d8c82c9d159f4ca98c4268d3b9477ea
kitkat1900 <moco1900@gmail.com>
2022-08-04 13:30:27 +0900

    単体テストコードの実装2

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :user do
-    nickname              {Faker::Name.initials(number: 2)}
+    nickname              {Faker::Name.initials}
     email                 {Faker::Internet.free_email}
     password              {Faker::Lorem.characters(min_alpha: 1, min_numeric: 1, number: 6)}
     password_confirmation {password}
