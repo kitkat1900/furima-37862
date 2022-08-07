8d884e709abb95d735c458f13dabd50dee92e1c3
kitkat1900 <moco1900@gmail.com>
2022-08-08 01:43:45 +0900

    エラーハンドリングの実装

--- app/javascript/index.js
+++ app/javascript/index.js
@@ -1,9 +1,9 @@
 function cul (){
   const itemPrice  = document.getElementById("item-price");
   itemPrice.addEventListener("keyup", () => {
-    const taxVal = itemPrice.value * 0.1;
+    const taxVal = Math.floor(itemPrice.value * 0.1);
     const addTaxPrice  = document.getElementById("add-tax-price");
-    const profitVal = itemPrice.value - (itemPrice.value * 0.1);
+    const profitVal = itemPrice.value - Math.floor(itemPrice.value * 0.1);
     const profit  = document.getElementById("profit");
     addTaxPrice.innerHTML = `${taxVal}`;
     profit.innerHTML = `${profitVal}`;
--- app/models/item.rb
+++ app/models/item.rb
@@ -8,9 +8,13 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
 
+  validates :item_name, presence: true
+  validates :description, presence: true
   validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
   validates :condition_id, numericality: { other_than: 1 , message: "can't be blank"}
   validates :shipping_method_id, numericality: { other_than: 1 , message: "can't be blank"}
   validates :ships_from_id, numericality: { other_than: 1 , message: "can't be blank"}
   validates :shipping_date_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :price, presence: true, numericality: { only_integer: true, greater_than: 299, less_than:10000000, message: 'には半角数字で¥300~¥9,999,999の間で入力してください'}
+  validates :image, presence: true
 end
