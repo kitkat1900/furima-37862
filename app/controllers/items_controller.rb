96444145c4ebae7b9d89871f3d39048068cf86ff
kitkat1900 <moco1900@gmail.com>
2022-08-22 15:30:00 +0900

    商品情報編集ページの作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -24,6 +24,13 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def edit
+    item = Item.find(params[:id])
+  end
+
+  def update
+  end
+
   def item_params
     params.require(:item).permit(:item_name, :description, :category_id, :condition_id,:shipping_method_id, :ships_from_id, :shipping_date_id, :price, :image).merge(user_id: current_user.id)
   end
