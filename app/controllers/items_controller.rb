9f34a697db6328ef6d21769409d6fad2c99ce64d
kitkat1900 <moco1900@gmail.com>
2022-08-22 17:36:17 +0900

    エラーハンドリングの実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!,only: [:new]
+  before_action :authenticate_user!,only: [:new, :edit]
 
   def index
     @items = Item.includes(:user).order("created_at DESC")
@@ -25,10 +25,19 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    item = Item.find(params[:id])
+    @item = Item.find(params[:id])
+    unless current_user == @item.user
+      redirect_to root_path
+    end
   end
 
   def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
   end
 
   def item_params
