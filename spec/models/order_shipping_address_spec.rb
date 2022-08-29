766b25419d480a05f213e6ced1a507c84fad67f0
kitkat1900 <moco1900@gmail.com>
2022-08-29 16:25:21 +0900

    単体テストコードの実装

---
+++ spec/models/order_shipping_address_spec.rb
@@ -0,0 +1,65 @@
+require 'rails_helper'
+
+RSpec.describe OrderShippingAddress, type: :model do
+  describe '購入情報の保存' do
+    before do
+      user = FactoryBot.create(:user)
+      item = FactoryBot.create(:item)
+      @order_shipping_address = FactoryBot.build(:order_shipping_address, item_id: item.id, user_id: user.id)
+      sleep 0.1 
+    end
+
+    context '内容に問題ない場合' do
+      it 'すべての値が正しく入力されていれば保存できること' do
+        expect(@order_shipping_address).to be_valid
+      end
+      it 'buildingは空でも保存できること' do
+        @order_shipping_address.building = nil
+        expect(@order_shipping_address).to be_valid
+      end
+    end
+
+    context '内容に問題がある場合' do
+      it 'tokenが空だと購入ができない' do
+        @order_shipping_address.token = nil
+        @order_shipping_address.valid?
+        expect(@order_shipping_address.errors.full_messages).to include("Token can't be blank")
+      end
+      it 'postal_codeが空だと保存できないこと' do
+        @order_shipping_address.postal_code = nil
+        @order_shipping_address.valid?
+        expect(@order_shipping_address.errors.full_messages).to include "Postal code can't be blank"
+      end
+      it 'postal_codeが半角のハイフンを含んだ正しい形式でないと保存できないこと' do
+        @order_shipping_address.postal_code = '1234567'
+        @order_shipping_address.valid?
+        expect(@order_shipping_address.errors.full_messages).to include('Postal code is invalid. Include hyphen(-)')
+      end
+      it 'prefecture_idに「---」が選択されている場合は保存できないこと' do
+        @order_shipping_address.prefecture_id = 1
+        @order_shipping_address.valid?
+        expect(@order_shipping_address.errors.full_messages).to include "Prefecture can't be blank"
+      end
+      it 'addressが空だと保存できないこと' do
+        @order_shipping_address.address = nil
+        @order_shipping_address.valid?
+        expect(@order_shipping_address.errors.full_messages).to include "Address can't be blank"
+      end
+      it 'phone_numberが空だと保存できないこと' do
+        @order_shipping_address.phone_number = nil
+        @order_shipping_address.valid?
+        expect(@order_shipping_address.errors.full_messages).to include "Phone number can't be blank"
+      end
+      it 'itemが紐付いていないと保存できないこと' do
+        @order_shipping_address.item_id = nil
+        @order_shipping_address.valid?
+        expect(@order_shipping_address.errors.full_messages).to include("Item can't be blank")
+      end
+      it 'userが紐付いていないと保存できない' do
+        @order_shipping_address.user_id = nil
+        @order_shipping_address.valid?
+        expect(@order_shipping_address.errors.full_messages).to include("User can't be blank")
+      end
+    end
+  end
+end
